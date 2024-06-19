0a4eff8317847729a7b854adf3cdc502851cd3e3
Rory Dowse <rorydowse@hotmail.com>
2024-06-19 15:45:11 -0700

    update js-arrays-1

--- course-content/03-javascript/code-drills/03-arrays/01-js-arrays-1/challenge-prompt.js
+++ course-content/03-javascript/code-drills/03-arrays/01-js-arrays-1/challenge-prompt.js
@@ -1,68 +1,56 @@
 // Working with Arrays
 
-
-
-
 // Create an array containing the integers from 0 to 3 and store it into a variable named `firstArray`
-
+const firstArray = [0, 1, 2, 3];
 // Create an array containing the integers from 4 to 7 and store it into a variable named `secondArray`
-
+const secondArray = [4, 5, 6, 7];
 // Create an array containing the integers from 8 to 11 and store it into a variable named `thirdArray`
-
+const thirdArray = [8, 9, 10, 11];
 // Create an array containing the integers from 12 to 15 and store it into a variable named `fourthArray`
-
+const fourthArray = [12, 13, 14, 15];
 
 // ------------------------------------------------
-console.log("============ firstArray ============")
+console.log(firstArray[0]);
+console.log(firstArray[1]);
+console.log(firstArray[2]);
+console.log(firstArray[3]);
+
 //
 // Console log `firstArray`
 
 // Individually console log the elements within `firstArray`
 // Write 4 console logs, 1 for each element
 
-
-
-
-
-
 // -------------------------------------------------
-console.log("============ secondArray ============")
+console.log(secondArray[0]);
+console.log(secondArray[1]);
+console.log(secondArray[2]);
+console.log(secondArray[3]);
+
 //
 // Console log `secondArray`
-
 
 // Individually console log the elements within `secondArray`
 // Write 4 console logs, 1 for each element
 
-
-
-
-
-
 // ------------------------------------------------
-console.log("============ thirdArray ============")
+console.log(thirdArray[0]);
+console.log(thirdArray[1]);
+console.log(thirdArray[2]);
+console.log(thirdArray[3]);
 //
 // Console log `thirdArray`
-
 
 // Individually console log the elements within `thirdArray`
 // Write 4 console logs, 1 for each element
 
-
-
-
-
-
 // -------------------------------------------------
-console.log("============ fourthArray ============")
+console.log(fourthArray[0]);
+console.log(fourthArray[1]);
+console.log(fourthArray[2]);
+console.log(fourthArray[3]);
 //
 // Console log `fourthArray`
 
-
 // Individually console log the elements within `fourthArray`
 // Write 4 console logs, 1 for each element
-
-
-
-
-
