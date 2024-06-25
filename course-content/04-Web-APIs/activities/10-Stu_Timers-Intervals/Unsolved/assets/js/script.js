155828953930a28168a6facfcd9a5801ca7fab1d
Rory Dowse <rorydowse@hotmail.com>
2024-06-25 13:58:52 -0700

    update timers-intervals

--- course-content/04-Web-APIs/activities/10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
+++ course-content/04-Web-APIs/activities/10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
@@ -1,33 +1,41 @@
+// get the elements that will display the countdown and main text
 const timerEl = document.getElementById("countdown");
 const mainEl = document.getElementById("main");
 
+// store the message in a vairable
 const message =
   "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.";
+// use the split method to create an array for each word
 const words = message.split(" ");
-
+// create a function for the countdown
 function countdown() {
-  const timeLeft = 5;
+  // initialize the countdown to start from 5
+  let timeLeft = 5;
 
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  // use the setInverval method
   const timeInterval = setInterval(function () {
-    // display time left
+    // display time left and update textConent of timerEl
     timerEl.textContent = `${timeLeft} seconds left`;
-    // decrease time left by 1
+    // decrement the time left
     timeLeft--;
-    // if time left is 0, stop counter and display message
-    if (timeLeft === 0) {
+    // if time left is less than 0, stop counter and start displaying message
+    if (timeLeft < 0) {
       clearInterval(timeInterval);
-    } else {
       displayMessage();
     }
+    // declare second part of setInterval function by updating every 1 second
   }, 1000);
 }
 
 // Displays the message one word at a time
+// declare the function to display the message
 function displayMessage() {
-  const wordCount = 0;
+  // initialize the word count at 0
+  let wordCount = 0;
 
   // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  // use setInterval method
   const msgInterval = setInterval(function () {
     // If there are no more words left in the message
     if (words[wordCount] === undefined) {
