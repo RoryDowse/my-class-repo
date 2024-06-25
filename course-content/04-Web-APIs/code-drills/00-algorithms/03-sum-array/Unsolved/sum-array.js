9e5176e515a1e136e0698139600962aa373c05fd
Rory Dowse <rorydowse@hotmail.com>
2024-06-25 09:09:57 -0700

    complete countdown and sum-array

--- course-content/04-Web-APIs/code-drills/00-algorithms/03-sum-array/Unsolved/sum-array.js
+++ course-content/04-Web-APIs/code-drills/00-algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,18 @@
 // Write code to add all the numbers in `arr` and return the total
+var arr = [3, 1, 5, 6];
+// array declared
 
-var sumArray = function(arr) {};
+var sumArray = function (arr) {
+  // function declared taking arr inpur
+  let total = 0;
+  // to find the total we must initialize first
+  for (let i = 0; i < arr.length; i++) {
+    // for loop for looping through array and attaching integers to i
+    total += arr[i];
+    // total is updated by adding values in arr
+  }
+  return total;
+  // the total is returned
+};
+sumArray();
+// function called providing updated values
