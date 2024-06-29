1f9c8ebfbd899b954752a4d24a7a8d9f3a25beb8
Rory Dowse <rorydowse@hotmail.com>
2024-06-29 16:14:47 -0700

    update blog.js

--- course-content/04-Web-APIs/challenge/Develop/assets/js/blog.js
+++ course-content/04-Web-APIs/challenge/Develop/assets/js/blog.js
@@ -1,9 +1,47 @@
 // TODO: Create a variable that selects the main element
-
+const getMainElement = document.getElementById("main");
 // TODO: Create a function that builds an element and appends it to the DOM
 
+function addArticle(blogData) {
+  const container = document.createElement("article");
+  const titleElement = document.createElement("h2");
+  const contentElement = document.createElement("p");
+  const usernameElement = document.createElement("p");
+
+  titleElement.textContent = blogData.title;
+  contentElement.textContent = blogData.content;
+  usernameElement.textContent = `Posted by: ${blogData.username}`;
+
+  container.setAttribute("class", "article");
+  titleElement.setAttribute("class", "");
+  contentElement.setAttribute("class", "");
+  usernameElement.setAttribute("class", "");
+
+  container.appendChild(titleElement);
+  container.appendChild(contentElement);
+  container.appendChild(usernameElement);
+
+  getMainElement.appendChild(container);
+}
+
 // TODO: Create a function that handles the case where there are no blog posts to display
-
+function noPost() {
+  const storedBlog = sessionStorage.getItem("blogData");
+  if (!storedBlog || storedBlog.trim() === "") {
+    alert("No posts are available");
+  }
+}
+noPost();
 // TODO: Create a function that reads from local storage and returns the data
-
+function renderBlogFromStorage() {
+  const storedBlog = sessionStorage.getItem("blogData");
+  if (storedBlog) {
+    const blogData = JSON.parse(storedBlog);
+    console.log("Blog data retrieved from localStorage:", blogData);
+    addArticle(blogData);
+  } else {
+    console.log("No blog data found in sessionStorage");
+  }
+}
 // TODO: Call the function to render the list of blog posts
+renderBlogFromStorage();
--- course-content/04-Web-APIs/challenge/Develop/assets/js/form.js
+++ course-content/04-Web-APIs/challenge/Develop/assets/js/form.js
@@ -1,6 +1,7 @@
 // TODO: Create a variable that selects the form element
 const getForm = document.getElementById("blogForm");
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
+
 function formSubmit(event) {
   event.preventDefault();
 
@@ -8,10 +9,10 @@ function formSubmit(event) {
   const title = document.getElementById("title").value;
   const content = document.getElementById("content").value;
 
-  if (!username || !title || !content) {
-    alert("Please complete the form.");
-    return;
-  }
+  // if (!username || !title || !content) {
+  //   alert("Please complete the form.");
+  //   return;
+  // }
 
   const blog = {
     username: username,
@@ -24,10 +25,11 @@ function formSubmit(event) {
 
   let redirectURL = "./blog.html";
 
-  const redirectPage = function (url) {
-    redirectURL = url;
+  const redirectPage = function (url, blogData) {
+    sessionStorage.setItem("blogData", JSON.stringify(blogData));
     location.assign(url);
   };
+  redirectPage(redirectURL, blog);
 }
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
